43e00abd351e6487c6624c6a48fb8fce7f744a6a
qicongsheng <875881559@qq.com>
2016-06-14 02:15:40 +0800

    修改设置title方法

--- 3D函数图像.py
+++ 3D函数图像.py
@@ -10,10 +10,9 @@ z = x**2 + y**2
 
 ax = plt.subplot(111, projection = '3d')
 ax.plot_surface(x, y, z, rstride=2, cstride=1, cmap=cm.jet, alpha=0.8)
-
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-plt.title('z = x^2 + y^2')
+ax.set_title('z = x^2 + y^2')
 
 plt.show()
