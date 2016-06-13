abaa5c4ce75961114e895ec94638331d3bd0e7a2
qicongsheng <875881559@qq.com>
2016-06-14 01:10:37 +0800

    修改3D图显示颜色

--- 3D函数图像.py
+++ 3D函数图像.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import numpy as np
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d
 
-x, y = np.mgrid[-20:20:100j, -20:20:100j]
+x, y = np.mgrid[-10:10:50j, -10:10:50j]
 z = x**2 + y**2
 
 ax = plt.subplot(111, projection = '3d')
-ax.plot_surface(x, y, z, rstride=2, cstride=1, cmap=plt.cm.coolwarm, alpha=0.8)
+ax.plot_surface(x, y, z, rstride=2, cstride=1, cmap=cm.jet, alpha=0.8)
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
