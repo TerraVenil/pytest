02100adde38dd74ee5e0e621c9e7e22a1844333e
qicongsheng <875881559@qq.com>
2016-06-13 22:56:25 +0800

    添加2D函数图像画图

---
+++ 2D函数图像.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+func = lambda x : x*2 + 1
+
+x = np.linspace(-100,100,300)
+y = func(x)
+
+plt.plot(x,y)
+plt.title('y = 2x + 1')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
