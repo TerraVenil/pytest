5ece6d78154f0dbc9e1dedde8e34ba200b041267
qicongsheng <875881559@qq.com>
2016-07-06 22:45:58 +0800

    添加注释

--- 获取股票数据.py
+++ 获取股票数据.py
@@ -3,14 +3,13 @@
 import tushare as ts
 import matplotlib.pyplot as plt
 
-df = ts.get_h_data('600000', start='2014-01-01', end='2014-06-16')
+df = ts.get_h_data('600000', start='1995-01-01', end='2016-06-16')
 
+#绘图
 x = df.index
 y = df.close
-
 plt.title('600000')
 plt.xlabel('date')
 plt.ylabel('price')
 plt.plot(x,y,"g-")
-
 plt.show()
