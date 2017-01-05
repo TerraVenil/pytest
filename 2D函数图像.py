7b030c1374d7b5d20d3cfb1a93d9fcba491a3409
qicongsheng <875881559@qq.com>
2017-01-05 10:24:00 +0800

    直接书写函数形式

--- 2D函数图像.py
+++ 2D函数图像.py
@@ -3,10 +3,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-func = lambda x : x**2 + 8
-
 x = np.linspace(-10,10,300)
-y = func(x)
+y = x**2 + 8
 
 plt.plot(x,y,'r-')
 plt.title('y = x^2 + 8')
